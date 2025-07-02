79d3b4e9f267ba0829fdca90132a026af90ad48d
hh92.jeong <jhh1472@nate.com>
2025-07-02 16:22:20 +0900

    lengthImplement

--- StringMatching/stringmatching_test.cpp
+++ StringMatching/stringmatching_test.cpp
@@ -7,6 +7,39 @@ using std::string;
 
 using namespace testing;
 
+class StirngMatchingFixture : public Test {
+public:
+    void checkMatchingLengthScore(int expectedScore, CompareStirng compareStr) {
+        StringMatching strMatching;
+        EXPECT_EQ(expectedScore, strMatching.getScore(compareStr));
+    }
+private:
+};
+
+TEST_F(StirngMatchingFixture, SameLength) {
+    CompareStirng compareStr = {"ASD","DSA"};
+    checkMatchingLengthScore(MAX_SCORE_FOR_STR_LEGNTH, compareStr);
+}
+
+TEST_F(StirngMatchingFixture, strAlong) {
+    CompareStirng compareStr = { "ASDD","DSA" };
+    checkMatchingLengthScore(45, compareStr);
+}
+
+TEST_F(StirngMatchingFixture, strAlongX2) {
+    CompareStirng compareStr = { "AA","B" };
+    checkMatchingLengthScore(0, compareStr);
+}
+
+TEST_F(StirngMatchingFixture, strBlong) {
+    CompareStirng compareStr = { "ASD","DSAD" };
+    checkMatchingLengthScore(45, compareStr);
+}
+TEST_F(StirngMatchingFixture, strBlongX2) {
+    CompareStirng compareStr = { "BB","CCCC" };
+    checkMatchingLengthScore(0, compareStr);
+}
+
 int main() {
     ::testing::InitGoogleMock();
     return RUN_ALL_TESTS();
